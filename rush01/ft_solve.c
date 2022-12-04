92ec688d4c5a98c135d74e3d1f49aa0e400e909e
Vinicius Careli <vcareli@gmail.com>
2022-12-04 10:26:22 -0500

    Add files via upload

--- rush01/ft_solve.c
+++ rush01/ft_solve.c
@@ -2,24 +2,50 @@
 
 int	ft_solve(char **tab, int size)
 {
-	/*
-	int	i;
-	int	x;
-	int	y;
+	int i;
 
-	i = 1;
-	y = 0;
-	while (y < size)
+	i = -1;
+	while (++i < size)
+		if (tab[i][size] + tab[i][size + 1] > size + 1
+			|| tab[i][size + 2] + tab[i][size + 3] > size + 1)
+			return (0);
+	return (ft_resolv(tab, 0, size));
+}
+
+int	ft_resolv(char **tab, int n, int size)
+{
+	int i;
+	int x;
+	int y;
+
+	if (n == size * size)
+		return (1);
+	i = 0;
+	x = n % size;
+	y = n / size;
+	while (++i <= size)
 	{
-		x = 0;
-		while (x < size)
+		tab[y][x] = i;
+		if (ft_is_valid_value(tab, x, y, size))
 		{
-			tab[y][x] = i;
-			x++;
+			if (ft_resolv(tab, n + 1, size))
+				return (1);
+			tab[y][x] = 0;
 		}
-		y++;
-		i++;
 	}
-	*/
+	tab[y][x] = 0;
+	return (0);
+}
+
+int	ft_is_valid_value(char **tab, int x, int y, int size)
+{
+	if (ft_is_duplicate(tab, x, y, size)
+		|| ft_height_col(tab, x, 1, size) > tab[x][size] || (y == size - 1
+		&& ft_height_col(tab, x, -1, size) != tab[x][size + 1])
+		|| ft_height_row(tab, y, 1, size) > tab[y][size + 2] || (x == size - 1
+		&& ft_height_row(tab, y, -1, size) != tab[y][size + 3]))
+		return (0);
 	return (1);
 }
+
+
