28d369696f9d0274a732e6255bee59f1fdcac584
Vinicius Careli <vcareli@gmail.com>
2022-12-13 12:38:29 -0500

    c8

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,17 @@
+#ifndef FT_H
+# define FT_H
+
+#include <unistd.h>
+
+# define	TRUE		1
+# define	FALSE		0
+# define	EVEN(nbr)	((nbr % 2 == 0) ? 1 : 0)
+# define	EVEN_MSG	"2 4 6..."
+# define	ODD_MSG		"3 5 7..."
+
+typedef struct t_bool
+{
+}	t_bool;
+
+
+#endif
